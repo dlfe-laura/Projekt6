d30ca7fefbf15a263aeb34502165a6aa33ba0c78
Laura <160341402+dlfe-laura@users.noreply.github.com>
2025-05-19 13:53:25 +0200

    commit

--- main/mainweb/functionality.js
+++ main/mainweb/functionality.js
@@ -1,16 +1,16 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
+//const hamburger = document.querySelector('.hamburger');
+//const navLinks = document.querySelector('.nav-links');
 
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('active'); // Toggle the 'active' class on the nav-links
-});
+//hamburger.addEventListener('click', () => {
+  //navLinks.classList.toggle('active'); // Toggle the 'active' class on the nav-links
+//});
 
 
 // Check
 
-  //const hamburger = document.querySelector('.hamburger');
-  //const menuOverlay = document.getElementById('menuOverlay');
+  const hamburger = document.querySelector('.hamburger');
+  const menuOverlay = document.getElementById('menuOverlay');
 
-  //hamburger.addEventListener('click', () => {
-    //menuOverlay.classList.toggle('show');
-  //});
+  hamburger.addEventListener('click', () => {
+    menuOverlay.classList.toggle('show');
+  });
