c21e0c0351fbaf140fcee5b1d37d58d12336c703
Laura <160341402+dlfe-laura@users.noreply.github.com>
2025-05-19 13:50:34 +0200

    coomit

--- main/mainweb/functionality.js
+++ main/mainweb/functionality.js
@@ -4,3 +4,13 @@ const navLinks = document.querySelector('.nav-links');
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active'); // Toggle the 'active' class on the nav-links
 });
+
+
+// Check
+
+  //const hamburger = document.querySelector('.hamburger');
+  //const menuOverlay = document.getElementById('menuOverlay');
+
+  //hamburger.addEventListener('click', () => {
+    //menuOverlay.classList.toggle('show');
+  //});
